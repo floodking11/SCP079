c743ef7600bb92c42cfa83e741a9d70ce0c59c4a
floodking11 <43711594+floodking11@users.noreply.github.com>
2018-09-30 01:07:24 +0200

    Update XWBOT.py

--- XWBOT.py
+++ XWBOT.py
@@ -20,7 +20,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == '!SCP173 ':
+    if message.content == '!SCP173':
         await client.send_message(message.channel,'SCP-173 is to be kept in a locked container at all times. When personnel must enter SCP-173 container, no fewer than 3 may enter at any time and the door is to be relocked behind them. At all times, two persons must maintain direct eye contact with SCP-173 until all personnel have vacated and relocked the container.')
     if message.content == '!SCP079':
         await client.send_message(message.channel,'SCP-079 is an Exidy Sorcerer microcomputer built in 1978. In 1981, its owner, █████ ██████ (deceased), a college sophomore attending ███, took it upon himself to attempt to code an AI. According to his notes, his plan was for the code to continuously evolve and improve itself as time went on. His project was completed a few months later, and after some tests and tweaks, █████ lost interest and moved on to a different brand of microcomputer. He left SCP-079 in his cluttered garage, still plugged in, and forgot about it for the next five years.')
